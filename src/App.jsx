1e183832b71a767ccf944293ab3fe6dd5525d976
Francisco Ripardo Maia Junior <57280475+FcoRipJr@users.noreply.github.com>
2026-09-16 15:51:38 +0000

    style: add bowser icon

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,7 @@ function App() {
       <header className="topbar">
         <div className="brand-wrap">
           <NavLink to="/" className="brand">
-            resu-me
+            Resu Me
           </NavLink>
         </div>
 
